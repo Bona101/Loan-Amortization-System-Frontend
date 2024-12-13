806f7b7b37ef1c9e54a84d438cf86e57d1e0ad3a
Bona101 <bonaventurembbahoffor@gmail.com>
2024-12-13 19:21:10 +0100

    feat: finish

--- newproject/api/admin.py
+++ newproject/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import profile, Transactions
 
-# Register your models here.
+# Registering the profile model
+@admin.register(profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'username', 'role', 'balance', 'loan_amount', 'loan_term')
+    search_fields = ('name', 'username')
+
+# Registering the Transactions model
+@admin.register(Transactions)
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'amount', 'state', 'next_payment', 'interest_on_loan', 'date')
+    list_filter = ('state', 'date')
+    search_fields = ('profile__name', 'amount')
